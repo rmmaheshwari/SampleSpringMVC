f0ea6022b1efe00795ceaa9d8c2cad3f47e84394
naveenjakad <naveenjakad@Naveens-MacBook-Pro-2.local>
2014-07-20 23:00:30 +0530

    cokie code

--- src/main/java/com/homeshop18/controller/HomePageController.java
+++ src/main/java/com/homeshop18/controller/HomePageController.java
@@ -1,7 +1,11 @@
 package com.homeshop18.controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,10 +14,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomePageController {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String welcome(ModelMap model) {
-		model.addAttribute("message", "Welcome to Home Page");
-
+	public String welcome(@CookieValue(value = "JSESSIONID") String fooCookie, HttpServletRequest request,HttpServletResponse response) {
+		Cookie cookie = new Cookie("JSESSIONID", "1234567890");
+		cookie.setDomain("localhost");
+		response.addCookie(cookie);
+		System.out.println(fooCookie);
 		return "index";
-
 	}
 }
